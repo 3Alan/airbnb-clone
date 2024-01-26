cc1907c75bfa23dd2fb85eff5fd7be49454e65e4
3Alan <1540703192@qq.com>
2024-01-26 21:30:21 +0800

    feat: add api url env

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,7 +6,7 @@ import { useUserStore } from '@/store/user';
 const isDev = process.env.NODE_ENV === 'development';
 
 const request = axios.create({
-  baseURL: isDev ? 'http://localhost:8081/api' : '/api'
+  baseURL: isDev ? 'http://localhost:8081/api' : `${process.env.EXPO_PUBLIC_API_URL}/api`
 });
 
 request.interceptors.request.use(
