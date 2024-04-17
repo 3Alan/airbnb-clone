d016de186ba6b546fa245e4636f483d630b35d77
paulw <paulw@myautobest.com>
2024-04-17 19:06:50 +0800

    chore: add unistyles

---
+++ src/constants/unistyles.ts
@@ -0,0 +1,14 @@
+import { UnistylesRegistry } from 'react-native-unistyles';
+
+import { defaultTheme } from './themes';
+
+type AppThemes = {
+  defaultTheme: typeof defaultTheme;
+};
+
+// override library types
+declare module 'react-native-unistyles' {
+  export interface UnistylesThemes extends AppThemes {}
+}
+
+UnistylesRegistry.addThemes({ defaultTheme });
